609e4ea94d90ac31ec876b673df4359d0a22bd9f
tanimorimizuki <mizuki.tanimori@rakus-partners.co.jp>
2020-03-17 13:57:32 +0900

    first comment

---
+++ src/main/java/com/example9/PersonalExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalExerciseApplication.class, args);
+	}
+
+}
